ce3b611037e8341df5a2c524a6794d0efc923e4a
Herut Yona <yonaherut@gmail.com>
2018-07-05 15:24:55 +0300

    folders

---
+++ js/services/misterEmail-service.js
@@ -0,0 +1,42 @@
+'use strict'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var emails = [{
+        subject: '',
+        body: '',
+        isRead: false,
+        sentAt: 0
+    },
+    {
+        subject: '',
+        body: '',
+        isRead: false,
+        sentAt: 0
+    },
+    {
+        subject: '',
+        body: '',
+        isRead: false,
+        sentAt: 0
+    },
+    {
+        subject: '',
+        body: '',
+        isRead: false,
+        sentAt: 0
+    }
+]
